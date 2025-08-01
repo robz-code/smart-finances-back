85127e73e0b53776866dd80b6ef1fedfcf656b26
cesarRz <cesarrz2000@gmail.com>
2025-08-01 01:36:05 -0600

    Refactor AccountRepository and AccountService to improve type safety by specifying the Account type in their class definitions.

--- app/repository/account_repository.py
+++ app/repository/account_repository.py
@@ -3,6 +3,6 @@ from sqlalchemy.orm import Session
 from app.entities.account import Account
 
 
-class AccountRepository(BaseRepository):
+class AccountRepository(BaseRepository[Account]):
     def __init__(self, db: Session):
         super().__init__(db, Account)
